cb3b27769d715005c147f71af39f283c589dea17
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2025-07-29 22:54:56 -0300

    primeira migration

--- back-end/src/controllers/AuthControllers.ts
+++ back-end/src/controllers/AuthControllers.ts
@@ -1,21 +1,7 @@
 import { JsonController, Post, Body, HttpCode } from 'routing-controllers';
 import { AuthService } from '../services/AuthService';
-import { LoginDTO } from '../dtos/LoginDTO';
-// import { RegisterDTO } from '../dtos/RegisterDTO';
 
 @JsonController('/auth')
 export class AuthController {
     private service = new AuthService();
-
-    @Post('/login')
-    @HttpCode(200)
-    login(@Body() body: LoginDTO) {
-        return this.service.login(body);
-    }
-
-    @Post('/register')
-    @HttpCode(201)
-    register(@Body() body: RegisterDTO) {
-        return this.service.register(body);
-    }
 }
