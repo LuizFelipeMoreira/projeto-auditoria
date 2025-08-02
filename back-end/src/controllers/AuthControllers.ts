d35b9632040a81f928314d781db51885534b0441
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2025-08-01 23:56:17 -0300

    iniciando controllers

--- back-end/src/controllers/AuthControllers.ts
+++ back-end/src/controllers/AuthControllers.ts
@@ -1,7 +1,11 @@
 import { JsonController, Post, Body, HttpCode } from 'routing-controllers';
 import { AuthService } from '../services/AuthService';
+import { Prisma } from '../generated/prisma';
 
 @JsonController('/auth')
 export class AuthController {
     private readonly service = new AuthService();
+
+    @Post('/user')
+    async getUser(@Body() body: Prisma.UserCreateInput) {}
 }
