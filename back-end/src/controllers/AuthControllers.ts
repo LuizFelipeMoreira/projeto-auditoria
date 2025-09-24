2367955598bd885ca625e458be590f5dbe9a357e
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2025-09-23 23:08:49 -0300

    retirando erro do controller de user

--- back-end/src/controllers/AuthControllers.ts
+++ back-end/src/controllers/AuthControllers.ts
@@ -1,18 +1,6 @@
-import {
-    BadRequestError,
-    Body,
-    HttpCode,
-    JsonController,
-    Post,
-} from 'routing-controllers';
+import { Body, HttpCode, JsonController, Post } from 'routing-controllers';
 import { AuthService } from '../services/AuthService';
-import { LoginRequestDTO } from '../dto/LoginDTO';
-
-interface UserRequest {
-    name: string;
-    email: string;
-    password: string;
-}
+import { LoginRequestDTO, LoginResponseDTO } from '../dto/LoginDTO';
 
 @JsonController('/auth')
 export class AuthController {
@@ -21,26 +9,12 @@ export class AuthController {
     @Post('/signup')
     @HttpCode(201)
     public async signup(@Body() body: LoginRequestDTO) {
-        const user = await this.authService.register(body);
-
-        console.log(user);
-
-        if (!user) {
-            throw new BadRequestError('Usuario ja existe');
-        }
-
-        return user;
+        return await this.authService.register(body);
     }
 
     @Post('/signin')
     @HttpCode(201)
-    public async signin(@Body() body: UserRequest) {
-        const user = await this.authService.login(body.email, body.password);
-
-        if (!user) {
-            throw new BadRequestError('Usuario ja existe');
-        }
-
-        return user;
+    public async signin(@Body() body: LoginResponseDTO) {
+        return await this.authService.login(body.email, body.password);
     }
 }
