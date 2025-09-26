b0ddd7e6bed46285982d90f444616d239a2e222c
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2025-09-26 16:12:49 -0300

    metodos de envio de email

--- back-end/src/services/EmailService.ts
+++ back-end/src/services/EmailService.ts
@@ -22,11 +22,37 @@ export class EmailService {
         });
     }
 
-    async sendEmail(): Promise<EmailSendResult> {
-        return await this.transporter.sendMail({});
+    async sendEmail(
+        name: string,
+        email: string,
+        to: string,
+        subject: string,
+        text: string,
+        html?: string
+    ): Promise<EmailSendResult> {
+        return await this.transporter.sendMail({
+            from: name + ' ' + email,
+            to,
+            subject,
+            text,
+            html,
+        });
     }
 
-    async notifyAdminSolicitation(): Promise<EmailSendResult> {
-        return await this.transporter.sendMail({});
+    async notifyAdminSolicitation(
+        name: string,
+        email: string,
+        to: string,
+        subject: string,
+        text: string,
+        html?: string
+    ): Promise<EmailSendResult> {
+        return await this.transporter.sendMail({
+            from: name + ' ' + email,
+            to,
+            subject,
+            text,
+            html,
+        });
     }
 }
